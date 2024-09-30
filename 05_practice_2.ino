7fa7de6f3e5bc9aa3d8c439f10739f52d8354487
xeoxjae <161583615+xeoxjae@users.noreply.github.com>
2024-09-30 11:31:26 +0900

    Add files via upload

---
+++ 05_practice_2.ino
@@ -0,0 +1,21 @@
+#define PIN_LED 7
+void setup() {
+  pinMode(PIN_LED, OUTPUT);
+}
+
+void loop() {
+  digitalWrite(PIN_LED,0);
+  digitalWrite(PIN_LED, LOW);
+  delay(1000);
+
+  for (int i = 0; i < 5; i++) {
+    digitalWrite(PIN_LED, HIGH); 
+    delay(100);                 
+    digitalWrite(PIN_LED, LOW); 
+    delay(100);                 
+  }
+
+  digitalWrite(PIN_LED, HIGH);
+  while (true) {
+  }
+}
